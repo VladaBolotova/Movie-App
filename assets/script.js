fe1088113347911fa53d4e1f376347f0b718a7d1
almoral323 <almoral323@gmail.com>
2022-11-03 18:43:53 -0400

    added starter code

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '126aa9a222msh511ea90b77f0d10p1b34d9jsnbf58338755b4',
+		'X-RapidAPI-Host': 'moviesdb5.p.rapidapi.com'
+	}
+};
+
+fetch('https://moviesdb5.p.rapidapi.com/om?t=Spider-Man', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
