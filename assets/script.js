9a50ff57cc8e0dbac2ad689f1dde4fd122386a95
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-11-05 12:07:48 -0400

    modifying html css javascript

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
+// global scope - scope 1
 var checkList = document.getElementsByClassName('d-check');
 for(i=0;i<checkList.length;i++){
-	console.log(checkList[i]);
-	var anchor = checkList[i].getElementsByClassName('anchor')[0];
-	console.log(anchor)
-	anchor.addEventListener('click', function(){
-		console.log(anchor)
-		if (anchor.classList.contains('visible'))
-		anchor.classList.remove('visible');
+	//for loop "local" scope - scope 2
+	var anchor = checkList[i].querySelector('.anchor');
+	anchor.addEventListener('click', function(event){
+		//event listener "local" scope - scope 3
+		if (event.target.parentElement.classList.contains('visible'))
+		event.target.parentElement.classList.remove('visible');
 		else
-		anchor.classList.add('visible');
+		event.target.parentElement.classList.add('visible');
 	}) 
 }
