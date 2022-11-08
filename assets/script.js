63ed41ae74c2e9a55965ef01a9ade64ac63a5a05
Vlada <111080921+VladaBolotova@users.noreply.github.com>
2022-11-07 19:22:38 -0500

    adding html

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,25 @@ for(i=0;i<checkList.length;i++){
 	var anchor = checkList[i].querySelector('.anchor');
 	anchor.addEventListener('click', function(event){
 		//event listener "local" scope - scope 3
-		if (event.target.parentElement.classList.contains('visible'))
-		event.target.parentElement.classList.remove('visible');
-		else
-		event.target.parentElement.classList.add('visible');
+		if (event.target.parentElement.classList.contains('visible')){
+			event.target.parentElement.classList.remove('visible');
+			// if there is an input with a type range then add a class with display none 
+		
+		}else{
+			event.target.parentElement.classList.add('visible');
+			// if there is an input with a type range then remove the class with display none 
+		}
 	}) 
 }
+
+$(".range-selector").click(function(){
+	var range=$(this).children("input")[0];
+	if($(range).hasClass("hidden")){
+		$(range).removeClass("hidden");
+	}else{
+		$(range).addClass("hidden");
+
+	}
+	
+
+})
