53b31c24c4c38fe4b74c817358b8301f44ac7ee0
[brian campero] <[brian_campero@yahoo.com]>
2022-11-14 17:13:43 -0500

    html

--- assets/watchLater.js
+++ assets/watchLater.js
@@ -5,11 +5,11 @@ function watchLater(params) {
 		var titlecon = document.createElement('h4')
 		var moviebox = document.createElement('div')
 		moviebox.classList.add("movie-box")
-		moviebox.setAttribute("id", "Halloween Ends") //setting the div ID of the movie boxes to the title of the movie
+		moviebox.setAttribute("id", "Halloween Ends") //setting the Div Id of the movie box to the title of the movie
 		imgcon.src ="https://m.media-amazon.com/images/M/MV5BZTg1Y2Q3MzctMDlkOS00OGE1LWE4MjUtNmJjNDNkZmM2YmVkXkEyXkFqcGdeQXVyMjY5ODI4NDk@._V1_.jpg"
 		titlecon.textContent = "Halloween Ends"
 		moviebox.appendChild(imgcon)
 		moviebox.appendChild(titlecon)
-		//moviebox.appendChild(description)
+		//moviebox.appendChild(description)  this is not needed for now
         $('#watch-container').append(moviebox)
 }
